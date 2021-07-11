1ace447f6bc102296adfc00da2f13cd0e503455e
Alexei Barashkov <alexei.barashkov@pandadoc.com>
2021-07-11 21:46:46 +0300

    test

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
 
+
+const client = await pool.connect();
+const result = await client.query(`
+  CREATE TABLE test_table(id serial PRIMARY KEY, name VArCHAR(50));
+
+  INSERT INTO test_table(name)
+    VALUES('test name');
+`);
+client.release();
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
