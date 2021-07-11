f8112152a5b8945297e8217137b0ab3524008ab3
Alexei Barashkov <alexei.barashkov@pandadoc.com>
2021-07-11 21:03:23 +0300

    postgress and static storage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const { Pool } = require('pg');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +10,24 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`!!Listening to port: ${port}`);
 });
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
